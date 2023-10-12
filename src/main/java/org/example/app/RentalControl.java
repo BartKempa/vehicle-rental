c93c42c5930f97b96189ace13c3c2f118a34910a
BartKempa <bartekkempa@gmail.com>
2023-10-12 09:24:35 +0200

    dodano exc do getOption

--- src/main/java/org/example/app/RentalControl.java
+++ src/main/java/org/example/app/RentalControl.java
@@ -1,5 +1,8 @@
 package org.example.app;
 
+import org.example.exception.NoSuchOptionException;
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class RentalControl {
@@ -25,21 +28,30 @@ public class RentalControl {
                 case ADD_CLIENT -> System.out.println("dodaj klienta");
                 case DISPLAY_CLIENTS -> System.out.println("wyświel klientów");
                 case FIND_VEHICLE -> System.out.println("znajd auto");
+                default -> System.out.println("Brak wybranej opcji, spróbuj raz jeszcze");
             }
         } while (option != Option.EXIT);
-
     }
 
     private Option getOption() {
         Option option = null;
-        int optionNumber = scanner.nextInt();
-        for (Option value : Option.values()) {
-            if (value.getNumber() == optionNumber) {
-                option = value;
+        boolean isNumCorrect = false;
+        while (!isNumCorrect){
+            try {
+                int optionNumber = scanner.nextInt();
+                option = Option.getOptionFromInt(optionNumber);
+                isNumCorrect =true;
+            } catch (NoSuchOptionException e) {
+                System.out.println(e.getMessage());
+            } catch (InputMismatchException e){
+                System.err.println("Wpisano wartość, która nie jest liczbą z podanego zakresu, wybierz ponownie: ");
+            } finally {
+                scanner.nextLine();
             }
         }
         return option;
     }
+
 
     private void printOptions() {
         System.out.println("Proszę wybrać opcję: ");
@@ -50,19 +62,19 @@ public class RentalControl {
     }
 
     private enum Option{
-        EXIT(1, "Wyjście z aplikacji"),
-        ADD_CAR(2, "Dodanie samochodu"),
-        ADD_TRUCK(3, "Dodanie vana"),
-        ADD_MOTORCYCLE(4, "Dodanie motocykla"),
-        DISPLAY_CARS(5, "Wyświetlenie samochodów osobowych"),
-        DISPLAY_TRUCKS(6, "Wyświetlenie vanów"),
-        DISPLAY_MOTORCYCLES(7, "Wyświetlenie motocykli"),
-        DELETE_CAR(8, "Usunięcie samochodu osobowego"),
-        DELETE_TRUCK(9, "Usunięcie vana"),
-        DELETE_MOTORCYCLE(10, "Usunięcie motocykla"),
-        ADD_CLIENT(11, "Dodanie klienta wypożyczalni"),
-        DISPLAY_CLIENTS(12, "Wyświetlenie klientów"),
-        FIND_VEHICLE(13, "Wyszukanie pojazdu");
+        EXIT(0, "Wyjście z aplikacji"),
+        ADD_CAR(1, "Dodanie samochodu"),
+        ADD_TRUCK(2, "Dodanie vana"),
+        ADD_MOTORCYCLE(3, "Dodanie motocykla"),
+        DISPLAY_CARS(4, "Wyświetlenie samochodów osobowych"),
+        DISPLAY_TRUCKS(5, "Wyświetlenie vanów"),
+        DISPLAY_MOTORCYCLES(6, "Wyświetlenie motocykli"),
+        DELETE_CAR(7, "Usunięcie samochodu osobowego"),
+        DELETE_TRUCK(8, "Usunięcie vana"),
+        DELETE_MOTORCYCLE(9, "Usunięcie motocykla"),
+        ADD_CLIENT(10, "Dodanie klienta wypożyczalni"),
+        DISPLAY_CLIENTS(11, "Wyświetlenie klientów"),
+        FIND_VEHICLE(12, "Wyszukanie pojazdu");
 
         private final int number;
 
@@ -85,6 +97,15 @@ public class RentalControl {
         public String toString() {
             return number +
                     ". " + description;
+        }
+        private static Option getOptionFromInt(int optionNumber) throws NoSuchOptionException {
+            Option option;
+            try {
+                option = Option.values()[optionNumber];
+            } catch (ArrayIndexOutOfBoundsException e){
+                throw new NoSuchOptionException("Brak opcji o numerze: " + optionNumber + " , wybierz ponownie.");
+            }
+            return option;
         }
     }
 
---
+++ src/main/java/org/example/exception/NoSuchOptionException.java
@@ -0,0 +1,7 @@
+package org.example.exception;
+
+public class NoSuchOptionException extends Exception{
+    public NoSuchOptionException(String message) {
+        super(message);
+    }
+}
