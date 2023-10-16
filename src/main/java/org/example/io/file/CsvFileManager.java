5704be3b0583e0aaeb491852e2861690627f3eab
BartKempa <bartekkempa@gmail.com>
2023-10-16 11:19:09 +0200

    import auta z csv

--- src/main/java/org/example/io/file/CsvFileManager.java
+++ src/main/java/org/example/io/file/CsvFileManager.java
@@ -1,14 +1,13 @@
 package org.example.io.file;
 
 import org.example.exception.DataExportException;
-import org.example.model.Rental;
-import org.example.model.Vehicle;
+import org.example.exception.DataImportException;
+import org.example.exception.InvalidDataException;
+import org.example.model.*;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.Collection;
-import java.util.Collections;
+import java.util.Scanner;
 
 public class CsvFileManager {
     private static final String VEHICLE_FILE_NAME = "Rental.csv";
@@ -28,7 +27,45 @@ public class CsvFileManager {
     }
 
     public void importVehicle(Rental rental){
+        try(Scanner scanner = new Scanner(new File(VEHICLE_FILE_NAME)))
+        {
+            while (scanner.hasNextLine()) {
+                String csvLine = scanner.nextLine();
+                Vehicle vehicle = createVehicleFromString(csvLine);
+                rental.addVehicle(vehicle);
+            }
+        } catch (IOException e) {
+            throw new DataImportException("Błąd wcztania danych z pliku " + VEHICLE_FILE_NAME);
+        }
+    }
 
+    private Vehicle createVehicleFromString(String csvLine) {
+        String[] split = csvLine.split(";");
+        String type = split[0];
+        if (Car.TYPE.equals(type)){
+            return createCarFromString(csvLine);
+        } if (Motorcycle.TYPE.equals(type)){
+            return createMotorcycleFromString(csvLine);
+        }  if (Truck.TYPE.equals(type)){
+            return createTruckFromString(csvLine);
+        }
+        throw new InvalidDataException("Nieznany typo pojazdu " + type);
+    }
+
+    private Vehicle createCarFromString(String csvLine) {
+        String[] split = csvLine.split(";");
+        String make = split[1];
+        String model = split[2];
+        int yearProduction = Integer.parseInt(split[3]);
+        String registrationNumber = split[4];
+        String transmission = split[5];
+        int engineSize = Integer.parseInt(split[6]);
+        String fuelType = split[7];
+        int kilometers = Integer.parseInt(split[8]);
+        String carCategory = split[9];
+        int doorsNumber = Integer.parseInt(split[10]);
+        int passengersNumber = Integer.parseInt(split[11]);
+        return new Car(make, model, yearProduction, registrationNumber, transmission, engineSize, fuelType, kilometers, carCategory, doorsNumber, passengersNumber);
     }
 
 }
